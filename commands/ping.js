df254bc6b6c43671b07fd3a1553815fbc8f0ead8
GamingBig <joerymunninghoff@gmail.com>
2021-09-14 15:09:53 +0200

    made a template and updated ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -20,6 +20,46 @@ module.exports = {
     execute(client, msg, args, curPrefix) {
         var msgTime = msg.createdTimestamp
         var curTime = Date.now()
-        console.log(msgTime - curTime)
+        var timeAlive = timeSince(new Date(curTime - client.uptime))
+        String.toString().toString().toString().toString().toString()
+        const embed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Ping and uptime')
+            .addFields(
+                [
+                    { name: 'Ping', value: Math.abs(msgTime - curTime) + 'ms', inline: true },
+                    { name: 'Uptime', value: timeAlive, inline: true }
+                ]
+            )
+
+        msg.channel.send({embeds: [embed]});
     },
 };
+
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = seconds / 31536000;
+
+    if (interval > 1) {
+        return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+        return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+        return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+        return Math.floor(interval) + " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+        return Math.floor(interval) + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+}
